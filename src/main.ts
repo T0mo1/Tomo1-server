fc40543366b2d1c12f8037297636143b20588699
Kinkin <hiimkien2702@gmail.com>
2025-12-23 06:58:21 -0500

    configured CORS for dev and prod

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module.js';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   // Enable CORS
-  app.enableCors(
-    {
-      origin: process.env.FRONTEND_URL,
-      credentials: true,
-    }
-  );
+  app.enableCors({
+    origin: configService.get<string[]>('ALLOWED_ORIGINS'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   // Global validation pipe
   app.useGlobalPipes(
